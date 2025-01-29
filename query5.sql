fa8424ff8f0e4687ac6da8abf33436237f9dbfc1
Wiktoria Müller <83777012+WiktoriaMuller@users.noreply.github.com>
2025-01-29 20:34:49 +0100

    Update query5.sql

--- query5.sql
+++ query5.sql
@@ -33,27 +33,3 @@ FROM Nauczyciel N
 INNER JOIN Lekcja L ON N.PESEL = L.PESELNauczyciela
 WHERE N.Nazwisko = 'Pyszczek'
 ORDER BY L.Data ASC 
-
--- Creating a view to count the number of students in each class.
-
-CREATE VIEW UczniowieWKlasie (NazwaKlasy, LiczbaUczniow)
-AS SELECT K.Nazwa "Nazwa klasy", COUNT(U.PESEL) "Liczba uczniów" 
-FROM Klasa K 
-LEFT JOIN Uczen U ON K.IDKlasy = U.IDKlasy
-GROUP BY K.Nazwa
-
--- Creating a view to count the number of lessons conducted by each teacher.
-
-CREATE VIEW LiczbaLekcjiNauczycieli (ImieNauczyciela, NazwiskoNauczyciela, LiczbaLekcji)
-AS SELECT N.Imie "Imię nauczyciela", N.Nazwisko "Nazwisko nauczyciela", COUNT(L.IDLekcji) "Liczba lekcji" 
-FROM Nauczyciel N 
-LEFT JOIN Lekcja L ON N.PESEL = L.PESELNauczyciela 
-GROUP BY N.Imie, N.Nazwisko, N.PESEL
-
--- Creating a view to count the number of lessons for each subject.
-
-CREATE VIEW LiczbaZajec (NazwaPrzedmiotu, LiczbaLekcji)
-AS SELECT P.Nazwa "Nazwa przedmiotu", COUNT(L.IDLekcji) "Liczba lekcji" 
-FROM Przedmiot P 
-LEFT JOIN Lekcja L ON P.IDPrzedmiotu = L.IDPrzedmiotu
-GROUP BY P.Nazwa
